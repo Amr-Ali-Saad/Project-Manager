70fcefad4b560978994f2e9cae811996f33c9e3e
Amr-Ali-Saad <alsbaghmrw.work@gmail.com>
2024-04-29 15:10:09 +0300

    try 1 : fix the img problem

--- src/components/NoProject.jsx
+++ src/components/NoProject.jsx
@@ -1,6 +1,6 @@
 export default function NoProject({setAddProject}){
     const imgClasses = "w-16 h-16 object-contain mx-auto"
-    const imgSRC= "src/assets/no-projects.png"
+    const imgSRC= "s/no-projects.png"
     const imgAlt = "no projects found"
     const divClasses = "mt-24 mr-auto ml-auto text-center w-2/3"
     const btnClasses = "px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
